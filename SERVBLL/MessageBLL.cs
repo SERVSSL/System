0c76a71aaa63d052f3e0ed82971ddccfd37171c9
Joel <joellister@users.noreply.github.com>
2019-10-06 15:37:00 +0100

    updated email footer

--- SERVBLL/MessageBLL.cs
+++ SERVBLL/MessageBLL.cs
@@ -19,7 +19,7 @@ namespace SERVBLL
 
 		private const string FROM = "noreply@system.servssl.org.uk";
 		private const string SERVER = "localhost";
-		public static string FOOTER = "\r\n\r\n\r\nThis message was sent from an unattended mailbox by the SERV SSL System.  Do not reply to this mail.  If you need to make contact, please use the Forum to PM Tristan Phillips.\r\n";
+		public static string FOOTER = "\r\n\r\n\r\nThis message was sent from an unattended mailbox by the SERV SSL System.  Do not reply to this mail.  If you need to make contact, please use the Forum http://servssl.org.uk/members/ \r\n";
 
 		public MessageBLL()
 		{
