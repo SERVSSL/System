1027445312ad65265523f1b75604f6f6203afde5
Joel <joellister@users.noreply.github.com>
2019-11-27 18:53:57 +0000

    getdata for edit WIP

--- SERVBLL/MilkLogBLL.cs
+++ SERVBLL/MilkLogBLL.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using SERVBLL.Mappers;
 using SERVBLL.ViewModel;
 using SERVDAL;
@@ -20,6 +21,18 @@ namespace SERVBLL
 			var runLog = _mapper.Map(model);
 			var prods = new List<int> { humanMilkProductId };
 			return new RunLogDAL().CreateRunLog(runLog, prods) > 0;
+		}
+
+		public MilkRunEditViewModel GetRunLogForEdit(string runLogId)
+		{
+			var dal = new RunLogDAL();
+			var dt = dal.GetMilkRunLog(int.Parse(runLogId));
+			dt.Rows[0].
+			foreach (DataColumn dataColumn in dt.Columns)
+			{
+				dataColumn.
+			}
+			return null;
 		}
 	}
 }
