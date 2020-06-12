5b1b26fc5f00254e31bea1a8ba20bb94c09cc5d5
Joel <joellister@users.noreply.github.com>
2020-06-12 09:38:54 +0100

    Added service to send SMS message using AQL rest api

---
+++ SERV.Utils/Sms/AqlMessageBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SERV.Utils.Sms
+{
+    public class AqlMessageBuilder
+    {
+        public string Build(string to, string from, string message)
+        {
+            var smsMessage = new SmsMessage {Message = message, Originator = from};
+            smsMessage.Destinations = to.Replace(" ","").Split(',');
+            var settings = new JsonSerializerSettings();
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            return JsonConvert.SerializeObject(smsMessage, settings);
+        }
+    }
+
+    public class SmsMessage
+    {
+        public string[] Destinations { get; set; }
+        public string Message { get; set; }
+        public string Originator { get; set; }
+    }
+}
---
+++ Tests/SmsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SERV.Utils.Sms;
+
+namespace Serv.Tests
+{
+    [TestFixture]
+    public class SmsTests
+    {
+        [Test]
+        public void GetCreditCountTest()
+        {
+            var classUnderTest = new AqlSmsService();
+
+            var result = classUnderTest.GetCreditCount();
+        }
+
+        [Test]
+        public void SendMessageTest()
+        {
+            var classUnderTest = new AqlSmsService();
+
+            var result = classUnderTest.SendMessage("447966143353,447581588277", "Joel TEST", "This is a test");
+        }
+    }
+}
