10f3ead447f88cdb2187bf0ac58915156f9c59b6
Joel <joellister@users.noreply.github.com>
2020-08-11 15:30:23 +0100

    update patch script to correct column name

--- SqlPatchScripts/AddDeliveryPostcodeForMilkRuns.sql
+++ SqlPatchScripts/AddDeliveryPostcodeForMilkRuns.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `SERV`.`RunLog` 
-ADD COLUMN `DeliveryPostcode` VARCHAR(8) NULL AFTER `DeliverToLocationID`;
+ADD COLUMN `DeliverToPostcode` VARCHAR(8) NULL AFTER `DeliverToLocationID`;
 
