60653339adafdb1b1fac55cd607752c634d38e77
Joel <joellister@users.noreply.github.com>
2020-09-18 17:10:37 +0100

    Added space to calendar

--- SERVWeb/js/Calendar.js
+++ SERVWeb/js/Calendar.js
@@ -277,14 +277,14 @@ function loadCalendar(memberId, userLevel)
 						if (adHoc) { icon = "<i class='icon icon-heart icon-star icon-green'></i> ";}
 						if (swapMarked) { strikeout="text-decoration: line-through"; icon = "<i class='icon icon-exclamation-sign icon-red'></i> ";}
 						if (calendarName == "") { icon = ""; }
-						var toAppend = '<div title="' + calendarName + '" class="calendarSlot calendarSlot' + calendarId +  spaceClass+ '" style="w--idth:150px">' + icon +
+						var toAppend = '<div title="' + calendarName + '" class="calendarSlot calendarSlot' + calendarId + spaceClass+ '" style="w--idth:150px">' + icon +
 									strong + italic + '<a style="' + strikeout + '" href="#" onclick="' +
 									'calSlotClicked(' + slotMemberId + ', ' + calendarId + ', \'' + calendarName + '\', \'' + shiftDate + '\', \'' + niceShiftDate + '\', ' + swapMarked + ');' + 
 									'">' + memberName + '</a></i></strong>' + 
 								'</div>';
 						if ((slotMemberId != memberId && userLevel < MANAGE_CALENDAR_USER_LEVEL) || simpleCalendar)
 						{
-							var toAppend = '<div title="' + calendarName + '" class="calendarSlot calendarSlot' + calendarId + '" style="w--idth:150px; ' + strikeout + '">' + icon + strong + italic + memberName  + '</i></strong></div>';
+							toAppend = '<div title="' + calendarName + '" class="calendarSlot calendarSlot' + calendarId + spaceClass + '" style="w--idth:150px; ' + strikeout + '">' + icon + strong + italic + memberName  + '</i></strong></div>';
 						}
 						$("#scheduledDay" + (day + 1)).append(toAppend);
 					}
